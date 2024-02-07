9bdb402ec6d2cc793281f5d326a92fb40fc643a2
Piyush-Pant2023 <piyushpant64@gmail.com>
2024-02-07 20:10:14 +0530

    basics

---
+++ practice.js
@@ -0,0 +1,11 @@
+let myArray = [1, 2, 3, 4, 5];
+
+// Access the first element
+let firstElement = myArray[0];
+
+// Access the last element
+let lastElement = myArray[myArray.length - 1];
+
+// Print the first and last elements
+console.log("First element:", firstElement);
+console.log("Last element:", lastElement);
